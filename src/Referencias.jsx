b85fd5fe79426261233b90c377a053885992b1ee
Luiz Fernando de Souza <104640200+luiz-fernand@users.noreply.github.com>
2022-10-07 12:44:43 -0300

    Pequeno ajuste no Nav e no Home e mudança completa no estilo das Referências

--- src/Referencias.jsx
+++ src/Referencias.jsx
@@ -7,58 +7,20 @@ const Referencias = () => {
         <div className='container'>
             <h1 className='referencias-titulo'>Referências</h1>
             <h2>Imagens</h2>
-            <div className='imagens'>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/backgroundHome.png'}  alt='Background Home'/>
-                    <p>Link</p>
+            <div className='imagens-ref'>
+                <p>Algumas imagens foram utilizadas sem a permissão de seus criadores, sem nenhum interesse comercial como foi proposto,
+                     e somente para fins estéticos, úteis e de aprendizado, deixo a referência dos mesmos:</p>
+                <div className='links'>
+                    <a className='link-ref' href='https://www.w3.org/html/logo/' target={'_blank'} rel='noreferrer'>W3C - HTML</a>
+                    <a className='link-ref' href='https://commons.wikimedia.org/wiki/File:CSS3_logo_and_wordmark.svg' target={'_blank'} rel='noreferrer'>Wikimedia - CSS</a>
+                    <a className='link-ref' href='https://commons.wikimedia.org/wiki/File:JavaScript-logo.png' target={'_blank'} rel='noreferrer'>Wikimedia - JS</a>
+                    <a className='link-ref' href='https://seeklogo.com/vector-logo/273749/node-js' target={'_blank'} rel='noreferrer'>Seeklogo - Node</a>
+                    <a className='link-ref' href='https://seeklogo.com/vector-logo/273845/react' target={'_blank'} rel='noreferrer'>Seeklogo - React</a>
+                    <a className='link-ref' href='https://commons.wikimedia.org/wiki/File:JSON_vector_logo.svg' target={'_blank'} rel='noreferrer'>Wikimedia - JSON</a>
+                    <a className='link-ref' href='https://git-scm.com/downloads/logos' target={'_blank'} rel='noreferrer'>Git - Git</a>
+                    <a className='link-ref' href='https://www.freepnglogos.com/images/logo-mysql-26353.html' target={'_blank'} rel='noreferrer'>Freepnglogos - MySQL</a>
+                    <a className='link-ref' href='https://br.freepik.com/vetores-premium/paisagem-de-montanha-preto-e-branco-com-floresta-de-pinheiros_7038778.htm' target={'_blank'} rel='noreferrer'>Freepik - Background</a>
                 </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/CSS-logo.png'}  alt='CSS'/>
-                    <p>Link</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/Git-logo.png'}  alt='Git'/>
-                    <p>Link</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/HTML-logo.png'}  alt='HTML'/>
-                    <p>Link</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/JS-logo.png'}  alt='JS'/>
-                    <p>Link</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/JSON-logo.png'}  alt='JSON'/>
-                    <p>Link</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/MySQL-logo.png'}  alt='MySQL'/>
-                    <p>Link</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/Node-logo.png'}  alt='Node'/>
-                    <p>Link</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/React-logo.png'}  alt='React'/>
-                    <p>Link</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/UP-logo-grande.png'}  alt='UP'/>
-                    <p>Própria</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/media/UP-logo-title.png'}  alt='UP'/>
-                    <p>Própria</p>
-                </div>
-                <div className='img'>
-                    <img src={process.env.PUBLIC_URL + '/UP-logo.ico'}  alt='UP'/>
-                    <p>Própria</p>
-                </div>
-            </div>
-            <div className='referencia'>
-
             </div>
         </div>
     )
